978299443dc9e4e5232fbf0bba00c03bf7b9b7c2
Mritunjay Kumar <101097898+Mritunjay882@users.noreply.github.com>
2023-04-29 12:10:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,5 @@ const status = document.getElementById("status");
 const enterBtn = document.getElementById("enterBtn");
 
 enterBtn.addEventListener("click", function() {
-	const h1 = document.createElement("h1");
-	h1.innerText = "Entered Metaverse";
-	status.replaceWith(h1);
+	status.innerText = "Entered Metaverse";
 });
